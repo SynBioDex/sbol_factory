41aa36ba34b45c590967d3850f5d466269028212
Bryan Bartley <bryan.a.bartley@raytheon.com>
2021-03-25 19:31:40 -0400

    Upgrade sbol dependency

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(name='opil',
       description='Open Protocol Interface Language (OPIL)',
       version='1.0b3',
       install_requires=[
-            'sbol3==1.0a6',
+            'sbol3==1.0a8',
             'rdflib>=5.0.0',
             'rdflib-jsonld>=0.5.0',
             'sparqlwrapper>=1.8.5',

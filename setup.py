09972848b282f2adb523dab5d2998cf442befc18
Bryan Bartley <bryan.a.bartley@raytheon.com>
2021-06-25 15:50:09 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ from setuptools import setup
 
 setup(name='sbol_factory',
       description='Ontology-driven data modeling',
-      version='1.0a4',
+      version='1.0a5',
       install_requires=[
-            'sbol3==1.0a10',
+            'sbol3>=1.0a10',
             'rdflib>=5.0.0',
             'rdflib-jsonld>=0.5.0',
             'sparqlwrapper>=1.8.5',

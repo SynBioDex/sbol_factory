ef3838f1f0723b934410a717264796df99384e78
Bryan Bartley <bryan.a.bartley@raytheon.com>
2021-09-23 13:21:37 -0700

    Create TexFactory

--- sbol_factory/__init__.py
+++ sbol_factory/__init__.py
@@ -1,3 +1,4 @@
 from .sbol_factory import SBOLFactory, Document, ValidationReport
+from .tex_factory import TexFactory
 from .uml_factory import UMLFactory
 from .shacl_validator import ShaclValidator
